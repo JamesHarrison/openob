65b5af15b4589f8034b01729edeccd302129e2ca
James Harrison <james@talkunafraid.co.uk>
2012-10-22 20:16:20 +0100

    Fixes missing dependency issue

--- openob/rtp/rx.py
+++ openob/rtp/rx.py
@@ -2,6 +2,7 @@ import gobject
 import pygst
 pygst.require("0.10")
 import gst
+import re
 
 class RTPReceiver:
   def __init__(self, caps='', audio_output='alsa', audio_device='hw:0', base_port=3000, encoding='celt', bitrate=96, jitter_buffer=150, jack_name='openob_rx'):
