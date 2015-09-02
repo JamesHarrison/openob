2069b59b23fdc02869476ffdb977e85e6dfc4589
James Harrison <james@talkunafraid.co.uk>
2015-09-02 23:56:43 +0100

    More progress, added skeletal openobctl cli tool for adding/deleting encoders, slight structural change

--- openob/node.py
+++ openob/node.py
@@ -5,6 +5,7 @@ import os
 import socket
 import psutil
 import time
+import multiprocessing as mp
 
 
 class Node(object):
@@ -13,22 +14,29 @@ class Node(object):
     A node wraps and manages all links associated with its configuration,
     and ensures they stay running.
     """
-    def __init__(self, node_id, config_hosts, audio_interfaces, log_level):
-        """Set up the node"""
+    def __init__(self):
+        self.encoders = {}
+        self.decoders = {}
+
+    def setup(self, node_id, config_hosts, audio_interfaces, log_level):
+        """Set up the node's basic configuration"""
         logging.basicConfig(level=log_level,
                             format='%(asctime)s:%(levelname)s: %(message)s')
         self.node_id = node_id
         self.audio_interfaces = audio_interfaces
-        logging.info('Starting OpenOB node ID "%s"' % self.node_id)
+        logging.info('Connecting to Zookeeper on "%s"' % config_hosts)
         self.zk = KazooClient(hosts=config_hosts)
         self.zk_base = "/openob/nodes/%s" % self.node_id
 
     def run(self):
         """Run the node; this method blocks and will run until interrupted"""
+        logging.info('Starting OpenOB node ID "%s"' % self.node_id)
         self.zk.start()
         self.zk.add_listener(self.zk_state_change_handler)
 
         self.zk.ensure_path(self.zk_base)
+        self.zk.ensure_path("%s/encoders" % self.zk_base)
+        self.zk.ensure_path("%s/decoders" % self.zk_base)
 
         # Clean up any existing audio interface records
         self.zk.delete("%s/audio_interfaces" % self.zk_base,
@@ -42,19 +50,58 @@ class Node(object):
             self.zk.create(path, json.dumps(dict(config)).encode('utf8'))
 
         self.set_status_data()
-        # Now we've configured all the info about this node, it's time to
-        # look at what Links are configured and set up our connection ends
-        self.update_links()
 
-    def update_links(self):
-        self.links = {}
-        # TODO: This probably isn't the way to go.
-        # /openob/nodes/:id/encoders/:eid/destinations/:did and same for rx
-        links = self.zk.get_children("/openob/links")
-        for link_path in links:
-            logging.debug("Inspecting %s for node involvement")
-            link_config_data = self.zk.get("link_path")
-            link_config = json.loads(str(link_config_data.decode('utf8')))
+        # Now we've configured all the info about this node, it's time to
+        # configure our encoders/decoders etc
+        encoder_ids = self.zk.get_children("%s/encoders" % self.zk_base,
+                                           watch=self.update_encoders)
+        for eid in encoder_ids:
+            data = self.zk.get('%s/encoders/%s' % (self.zk_base, eid))
+            config = json.loads(str(data.decode('utf8')))
+            self.setup_encoder(eid, config)
+
+        decoder_ids = self.zk.get_children("%s/decoders" % self.zk_base,
+                                           watch=self.update_decoders)
+        for did in decoder_ids:
+            data = self.zk.get('%s/decoders/%s' % (self.zk_base, did))
+            config = json.loads(str(data.decode('utf8')))
+            self.setup_decoder(did, config)
+
+        # At this point we've got a bunch of processes representing our
+        # configured encoders and decoders, all started.
+        self.monitor_processes()
+
+    def monitor_processes(self):
+        logging.warn("Would now start monitoring running encoders/decoders.")
+        logging.fatal("But I'm not going to because it's not implemented yet.")
+        for eid, enc in self.encoders.items():
+            enc.join()
+        for did, dec in self.decoders.items():
+            dec.join()
+
+    def setup_encoder(self, eid, config):
+        """Set up a new encoder, or re-create an existing one with a new
+        configuration"""
+        if self.encoders[eid]:
+            # FIXME: Probably should be nicer about this. Maybe.
+            self.encoders[eid].terminate()
+        self.encoders[eid] = mp.Process(target=self.run_encoder(config))
+        self.encoders[eid].start()
+
+    def setup_decoder(self, did, config):
+        pass
+
+    def update_encoders(self):
+        pass
+
+    def update_decoders(self):
+        pass
+
+    def add_encoder(self, eid, config):
+        logging.warn("Adding encoder with ID %s and config %s" % (eid, str(config)))
+
+    def delete_encoder(self, eid):
+        logging.warn("Deleting encoder with ID %s" % eid)
 
     def set_status_data(self):
         # thisproc = psutil.Process(os.getpid())
