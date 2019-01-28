5981878ec41807a1ee86c8719ff889c11c9743de
James Harrison <james.harrison@gigaclear.com>
2019-01-28 21:17:02 +0000

    Cutting a new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = ''
 with open('README.md', 'r') as f:
     long_description = f.read()
 setup(name='OpenOB',
-      version='4.0.2',
+      version='4.0.3',
       long_description=long_description,
       long_description_content_type='text/markdown',
       description='Broadcast audio over IP codec built with PyGST',
